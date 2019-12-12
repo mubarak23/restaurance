5228517aec16c7e42567b1dcdd9f07cb61314a64
mubarak23 <mubarakaminu340@gmail.com>
2019-12-12 13:03:02 +0100

    ft(auth):set password mutator for bcrypt[progress]

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,8 +29,9 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected $redirectTo = '/';
     /**
      * Create a new controller instance.
      *
@@ -67,7 +68,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => $data['password']
         ]);
     }
 }
