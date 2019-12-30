0f3399aa62fbc4aae108d542cb4720a7b056cc79
mubarak23 <mubarakaminu340@gmail.com>
2019-12-30 17:39:21 +0100

    ft(review):create MRC for review feature[progress]

---
+++ database/migrations/2019_12_30_163342_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('review');
+            $table->float('rating', 2, 1);
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
